ffe5b610cd082bdcf135a230f717729338dc24c1
Jerry <zzherui54@gmail.com>
2024-05-24 14:41:22 +0800

    modify main function

--- client/main.go
+++ client/main.go
@@ -24,6 +24,109 @@ const proxyHost = "100.0.0.1"
 const proxyPort = "30000"
 const HttpDataLen = 1310
 
+func main() {
+	roundTripper := &http3.RoundTripper{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+		QuicConfig: &quic.Config{
+			EnableDatagrams: true,
+		},
+		EnableDatagrams: true,
+		ReqId:           0,
+	}
+	client := &http.Client{
+		Transport: roundTripper,
+	}
+	defer roundTripper.Close()
+	roundTripper.InitialMap()
+	tapconfig := water.Config{
+		DeviceType: water.TUN,
+		PlatformSpecificParams: water.PlatformSpecificParams{
+			Name: "tun0",
+		},
+	}
+	ifce, err := water.New(tapconfig)
+	if err != nil {
+		utils.ErrorPrintf("create new tun interface err:%v\n", err)
+	}
+	setRoute()
+	ProxyManager := make(map[string]http3.Datagrammer)
+	//test, temporary variable
+	var src, dst *net.UDPAddr
+
+	p := &proxy.IPreorder{
+		ReorderedPacket: make(map[int][]byte),
+		FinishAssemble:  false,
+	}
+	//uplink
+	go func() {
+		for {
+			buf := make([]byte, 1500)
+			n, err := ifce.Read(buf)
+			if err != nil {
+				utils.ErrorPrintf("tun read err:%v\n", err)
+			}
+			utils.DebugPrintf("--------------------uplink read %d byte.--------------------\n", n)
+			if IsIPv4(buf[:n]) && IsUDP(buf[:n]) {
+				fragment := p.CheckFragment(buf[:n])
+				if fragment {
+					if p.FinishAssemble {
+						buf = p.Packet
+						n = len(p.Packet)
+					} else {
+						continue
+					}
+				}
+				targetIP := ParseTargetIP(buf[:20])
+				targetPort := ParseTargetPort(buf[:28])
+				fiveTuple := targetIP + ":" + targetPort
+				sourceIP := ParseSourceIP(buf[:20])
+				sourcePort := ParseSourcePort(buf[:28])
+				fiveTuple += ","
+				fiveTuple += sourceIP + ":" + sourcePort
+				d, ok := ProxyManager[fiveTuple]
+				if !ok {
+					// do a proxy request and get the datagrammer.
+					id, _ := doProxyReq(client, targetIP, targetPort)
+					str := roundTripper.GetReqStream(id)
+					d, _ = str.Datagrammer()
+					ProxyManager[fiveTuple] = d
+					// set the udp addr
+					src, dst = setUDPaddr(buf[:28])
+					// create the downlink go routine
+					go downlink(d, src, dst, ifce)
+				}
+				if n > HttpDataLen {
+					buf = buf[28:n]
+					n = n - 28
+					var i int
+					for i = 0; i+HttpDataLen < n; i = i + HttpDataLen {
+						j := i + HttpDataLen
+						data := make([]byte, HttpDataLen)
+						copy(data, buf[i:j])
+						data[HttpDataLen] = 0xff // make a mark
+						d.SendMessage(data)
+					}
+					d.SendMessage(buf[i:n])
+				} else {
+					err := d.SendMessage(buf[28:n])
+					if err != nil {
+						fmt.Printf("send Message err:%v\n", err)
+					}
+				}
+				p.FinishAssemble = false
+				p.Packet = nil
+
+			}
+		}
+	}()
+	for {
+
+	}
+
+}
+
 func doProxyReq(client *http.Client, targetHost string, targetPort string) (int, error) {
 	URL := "https://"
 	URL += proxyHost
@@ -187,107 +290,4 @@ func downlink(d http3.Datagrammer, appClient, appServer *net.UDPAddr, ifce *wate
 			}
 		}
 	}
-}
-
-func main() {
-	roundTripper := &http3.RoundTripper{
-		TLSClientConfig: &tls.Config{
-			InsecureSkipVerify: true,
-		},
-		QuicConfig: &quic.Config{
-			EnableDatagrams: true,
-		},
-		EnableDatagrams: true,
-		ReqId:           0,
-	}
-	client := &http.Client{
-		Transport: roundTripper,
-	}
-	defer roundTripper.Close()
-	roundTripper.InitialMap()
-	tapconfig := water.Config{
-		DeviceType: water.TUN,
-		PlatformSpecificParams: water.PlatformSpecificParams{
-			Name: "tun0",
-		},
-	}
-	ifce, err := water.New(tapconfig)
-	if err != nil {
-		utils.ErrorPrintf("create new tun interface err:%v\n", err)
-	}
-	setRoute()
-	ProxyManager := make(map[string]http3.Datagrammer)
-	//test, temporary variable
-	var src, dst *net.UDPAddr
-
-	p := &proxy.IPreorder{
-		ReorderedPacket: make(map[int][]byte),
-		FinishAssemble:  false,
-	}
-	//uplink
-	go func() {
-		for {
-			buf := make([]byte, 1500)
-			n, err := ifce.Read(buf)
-			if err != nil {
-				utils.ErrorPrintf("tun read err:%v\n", err)
-			}
-			utils.DebugPrintf("--------------------uplink read %d byte.--------------------\n", n)
-			if IsIPv4(buf[:n]) && IsUDP(buf[:n]) {
-				fragment := p.CheckFragment(buf[:n])
-				if fragment {
-					if p.FinishAssemble {
-						buf = p.Packet
-						n = len(p.Packet)
-					} else {
-						continue
-					}
-				}
-				targetIP := ParseTargetIP(buf[:20])
-				targetPort := ParseTargetPort(buf[:28])
-				fiveTuple := targetIP + ":" + targetPort
-				sourceIP := ParseSourceIP(buf[:20])
-				sourcePort := ParseSourcePort(buf[:28])
-				fiveTuple += ","
-				fiveTuple += sourceIP + ":" + sourcePort
-				d, ok := ProxyManager[fiveTuple]
-				if !ok {
-					// do a proxy request and get the datagrammer.
-					id, _ := doProxyReq(client, targetIP, targetPort)
-					str := roundTripper.GetReqStream(id)
-					d, _ = str.Datagrammer()
-					ProxyManager[fiveTuple] = d
-					// set the udp addr
-					src, dst = setUDPaddr(buf[:28])
-					// create the downlink go routine
-					go downlink(d, src, dst, ifce)
-				}
-				if n > HttpDataLen {
-					buf = buf[28:n]
-					n = n - 28
-					var i int
-					for i = 0; i+HttpDataLen < n; i = i + HttpDataLen {
-						j := i + HttpDataLen
-						data := make([]byte, HttpDataLen)
-						copy(data, buf[i:j])
-						data[HttpDataLen] = 0xff // make a mark
-						d.SendMessage(data)
-					}
-					d.SendMessage(buf[i:n])
-				} else {
-					err := d.SendMessage(buf[28:n])
-					if err != nil {
-						fmt.Printf("send Message err:%v\n", err)
-					}
-				}
-				p.FinishAssemble = false
-				p.Packet = nil
-
-			}
-		}
-	}()
-	for {
-
-	}
-
 }
